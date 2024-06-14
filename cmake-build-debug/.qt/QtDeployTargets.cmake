b6b2e1567d3b402c4d208eff923c7f01e612b0f3
Maksym <maximosinskiy@gmail.com>
2024-06-14 21:56:30 +0300

    Initial commit

---
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_password_generator_qt_FILE A:/password_generator_qt/cmake-build-debug/password_generator_qt.exe)
+set(__QT_DEPLOY_TARGET_password_generator_qt_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
